e25a37fad3501f1817bdf0ef25632c35e82e495f
Max Williams <max@maximile.net>
2011-09-16 00:26:59 +0100

    Level progress

--- Classes/Pickups/Shotgun.m
+++ Classes/Pickups/Shotgun.m
@@ -6,7 +6,7 @@
 	if ([super initWithPosition:position] == nil) return nil;
 
 	Texture *texture = [Texture textureNamed:@"MainSprites.psd"];
-	sprite = [[Sprite alloc] initWithTexture:texture texRect:pixelRectMake(72, 32, 16, 16)];
+	sprite = [[Sprite alloc] initWithTexture:texture texRect:pixelRectMake(64, 32, 16, 16)];
 	
 	return self;
 }
