2f09a84c346dce543c5f3eaae3b0a39e2972d2ed
Max Williams <max@maximile.net>
2011-09-21 00:13:07 +0100

    Parasprite infestation

--- Classes/Enemies/Parasprite.m
+++ Classes/Enemies/Parasprite.m
@@ -1,14 +1,16 @@
 #import "Parasprite.h"
 #import "RandomTools.h"
+#import "Player.h"
 
 @implementation Parasprite
 
 - (id)initWithPosition:(pixelCoords)position {
-	if ([super init]==nil) return nil;
+	if ([super initWithPosition:position]==nil) return nil;
 	
 	body = cpBodyNew(2, INFINITY);
 	cpBodySetPos(body, cpv(position.x, position.y));
 	cpBodySetUserData(body, self);
+	cpBodySetVelLimit(body, 50);
 	
 	shape = cpCircleShapeNew(body, 4, cpvzero);
 	cpShapeSetCollisionType(shape, [self class]);
@@ -37,9 +39,13 @@
 	pixelCoords pixelPos = self.pixelPosition;
 	
 	// draw wings
+	float flapRate = 24.0;
+	if (self.body->f.y > 2000.0) {
+		flapRate = 64.0;
+	}
 	pixelCoords wingPos = pixelCoordsMake(pixelPos.x, pixelPos.y + 6);
 	Sprite *wingsSprite;
-	if (cpfsin(64.0*(game.fixedTime + self.objectPhase)) > 0)
+	if (cpfsin(flapRate*(game.fixedTime + self.objectPhase)) > 0)
 		wingsSprite = wingsOneSprite;
 	else
 		wingsSprite = wingsTwoSprite;
@@ -50,10 +56,28 @@
 	[feetSprite drawAt:pixelCoordsMake(pixelPos.x, pixelPos.y -4)];
 }
 
+- (void)update:(Game *)game {
+	// set target - chase player if near enough, else return home
+	cpVect target = cpv(startingPosition.x, startingPosition.y);
+	Player *player = game.player;
+	if (player && cpvnear(player.position, self.position, 100)) {
+		target = player.position;
+	}
+	
+	// apply force towards target, with strenth varying periodically
+	float phase = 2.0*M_PI*self.objectPhase;
+	float enthusiasm = cpfsin(2.0*game.fixedTime + phase) * 0.5 + 0.5;
+	cpVect gravityForce = cpv(0, 2000);
+	cpVect targetForce = cpvsub(target, self.position);
+	if (targetForce.x == 0 && targetForce.y == 0) targetForce = cpv(0,1);
+	targetForce = cpvnormalize(targetForce);
+	targetForce = cpvmult(targetForce, enthusiasm * 500);
+	cpBodySetForce(body, cpvadd(targetForce, gravityForce));
+}
+
 - (void)addToSpace:(cpSpace *)space {
 	cpSpaceAddBody(space, body);
 	cpSpaceAddShape(space, shape);
-	cpBodySetForce(body, cpv(0, 2000));
 }
 
 - (void)removeFromSpace:(cpSpace *)space {
